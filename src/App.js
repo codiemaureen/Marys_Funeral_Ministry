c36e2289ec2ec2ac3e99a971238a0b52ea87d6e5
Codiemaureen <codiestover.cs@gmail.com>
2024-08-12 17:52:19 -0400

    Removing navBar import from app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
 import Reviews from "./components/Reviews";
 
 function App() {
