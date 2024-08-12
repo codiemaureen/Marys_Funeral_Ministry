41d2d82b1b63d79123f33437ab7254415c3fc533
Codiemaureen <codiestover.cs@gmail.com>
2024-08-12 16:59:46 -0400

    Adding Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer><a target="_blank" rel="noreferrer" href="https://icons8.com/icon/BbVmpdYi9O6H/funeral">Funeral</a> icon by <a target="_blank" rel="noreferrer"
+      href="https://icons8.com">Icons8</a></footer>
+    </div>
+  )
+}
+
+export default Footer
