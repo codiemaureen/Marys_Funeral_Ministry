08842fb867a95a256e0c117344847a0337f47303
Codiemaureen <codiestover.cs@gmail.com>
2024-08-12 17:50:50 -0400

    Adding Review Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import Reviews from "./components/Reviews";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Reviews />
       <Footer />
     </div>
   );
---
+++ src/components/Reviews.js
@@ -0,0 +1,12 @@
+const Reviews = () => {
+  return (
+    <div>
+     <h2>
+      Google Review API
+     </h2>
+     <a href="https://developers.google.com/my-business/content/review-data">Google Business Review API</a>
+    </div>
+  )
+}
+
+export default Reviews
