c54d25f76b91e82753f89aef07fa3750fa1283c2
Codiemaureen <codiestover.cs@gmail.com>
2024-08-14 17:43:29 -0400

    Adding Contact Form Component

---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Form</h1>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
+import Contact from "./Contact"
+
 const Footer = () => {
   return (
     <div>
-      <footer><a target="_blank" rel="noreferrer" href="https://icons8.com/icon/BbVmpdYi9O6H/funeral">Funeral</a> icon by <a target="_blank" rel="noreferrer"
-      href="https://icons8.com">Icons8</a></footer>
+      <footer>
+        <Contact />
+        <div className="iconCredit">
+          <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/BbVmpdYi9O6H/funeral">Funeral</a> icon by <a target="_blank" rel="noreferrer"
+          href="https://icons8.com">Icons8</a>          
+        </div>
+      </footer>
     </div>
   )
 }
